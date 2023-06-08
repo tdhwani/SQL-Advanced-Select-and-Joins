46bb51e271081ddca7adf2b2437eb74c353338d8
tdhwani <64563255+tdhwani@users.noreply.github.com>
2023-06-07 21:57:38 -0400

    Create solution.sql

---
+++ Consecutive Numbers/solution.sql
@@ -0,0 +1,11 @@
+select 
+  distinct l2.num as ConsecutiveNums 
+from 
+  Logs l1, 
+  Logs l2, 
+  Logs l3 
+where 
+  l2.id = l1.id + 1 
+  and l3.id = l2.id + 1 
+  and l1.num = l2.num 
+  and l1.num = l3.num;
